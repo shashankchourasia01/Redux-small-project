28ddd0f9d0c51028e4620b5e572df8a9f5c22331
shashank chourasia01 <shashankchourasia0829@gmail.com>
2025-12-28 13:07:19 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full text-white bg-gray-950'>
+      
+      
+      
+      </div>
+  )
+}
+
+export default App
